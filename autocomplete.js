ba08f5ed6f5a299cca662dc94f09597cbfcadf6e
Chris Skilton <chriss@brandwatch.com>
2016-03-01 15:16:06 +0000

    use underscore debounce

--- autocomplete.js
+++ autocomplete.js
@@ -6,29 +6,39 @@ const autocomplete = options => {
     let latestValue = '';
     let latestFetch;
 
-    function debounce(func, delay) {
-        let time;
-        let timeout;
+    function debounce(func, wait, immediate) {
+        var timeout,
+            args,
+            context,
+            timestamp,
+            result;
+
+        var later = function() {
+          var last = Date.now() - timestamp;
+
+          if (last < wait && last >= 0) {
+            timeout = setTimeout(later, wait - last);
+          } else {
+            timeout = null;
+            if (!immediate) {
+              result = func.apply(context, args);
+              if (!timeout) context = args = null;
+            }
+          }
+        };
 
         return function() {
-            let args = arguments;
-            let timeelapsed = (Date.now() - time);
+          context = this;
+          args = arguments;
+          timestamp = Date.now();
+          var callNow = immediate && !timeout;
+          if (!timeout) timeout = setTimeout(later, wait);
+          if (callNow) {
+            result = func.apply(context, args);
+            context = args = null;
+          }
 
-            if (time &&  timeelapsed < delay) {
-                if (timeout) {
-                    clearTimeout(timeout);
-                }
-
-                timeout = setTimeout(function() {
-                    func.apply(null, args);
-                }, timeelapsed - delay);
-                time = Date.now();
-
-                return;
-            }
-
-            func.apply(null, args);
-            time = Date.now();
+          return result;
         };
     }
 
@@ -73,5 +83,5 @@ const autocomplete = options => {
         };
 
         latestFetch.send(null);
-    }), 600);
+    }), 600, true);
 }
